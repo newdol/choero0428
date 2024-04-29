a1b9f7a33942354ed3985cc238995970e524a149
newdol <163724533+newdol@users.noreply.github.com>
2024-04-28 17:27:02 -0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const { exec, execSync } = require('child_process');
 const port = process.env.SERVER_PORT || process.env.PORT || 3000;        
-const UUID = process.env.UUID || '23e8ad8c-1c3c-4ee7-a29a-cd8e0a45395c'; //若需要改UUID，需要在config.json里改为一致
+const UUID = process.env.UUID || 'bff54b8a-abb5-4e3d-9f03-56e1c283070e'; //若需要改UUID，需要在config.json里改为一致
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nezha.xiaopang66.xyz';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '443';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
-const NEZHA_KEY = process.env.NEZHA_KEY || 'GpKB2veAtyVzr98XV1';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choero.648745.tk';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZWIzMTVmZTE1NmYxZTk5MmNkOGM1NDI3MTczOTdmODMiLCJ0IjoiZTA5YjRlMTMtZDM0MS00Y2UzLWI4OWMtZGI3MTAwNTZmZjYwIiwicyI6Ik9EVTRZelEwTldJdFl6RTROQzAwT0dNMExXSTVOMll0TW1WbFptSXlZVFJtTWpRMyJ9';
+const NEZHA_KEY = process.env.NEZHA_KEY || 'ePG6ENRx3CVqj5Pd0U';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo0428.jjjjjjjjjjjjjjj.cf';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZWIzMTVmZTE1NmYxZTk5MmNkOGM1NDI3MTczOTdmODMiLCJ0IjoiZjVkOTAwMWQtOTU2Ni00OWZiLTkyYTktMDBiNmNkZTMwMjkxIiwicyI6Ik1URTJOV1l4Tm1RdE56Wm1NUzAwTVRJMUxXRmxOREV0TTJSaU5UQXdNVFUzTVRaaSJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
